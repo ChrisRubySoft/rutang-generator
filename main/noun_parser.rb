4f3bdadb425ad9bbe9abe3f8d90f284dad9f01ab
Lucas Rabenstein-Bolufer <lucaspiboy@gmail.com>
2026-09-19 22:26:13 -0400

    added noun parser

--- main/noun_parser.rb
+++ main/noun_parser.rb
@@ -1,4 +1,8 @@
 
-nouns_file = File.open('english-nouns.txt')
-noun_list = nouns_file.readlines.map(&:chomp)
-puts noun_list
+def noun_parser(name)
+    nouns_file = File.open('english-nouns.txt')
+    noun_list = nouns_file.readlines.map(&:chomp)
+
+    name_sum = name.bytes.sum
+    return noun_list[name_sum % noun_list.length]
+end
